dbae9c80cdb5c9eccb44badc0acec0b395f7193b
Stas <stanislavkviz@mail.ru>
2020-04-16 22:04:15 +0300

    Creation of two new controllers and removing the test controller

---
+++ src/main/java/ru/kviz/springcourse/controllers/SecondController.java
@@ -0,0 +1,12 @@
+package ru.kviz.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit(){
+    return "second/exit";
+    }
+}
