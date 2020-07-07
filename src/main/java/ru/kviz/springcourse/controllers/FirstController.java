818d9b4a9aec9858428c29297210b027312624b7
Stas <stanislavkviz@mail.ru>
2020-07-07 13:28:04 +0300

    adding annotation @RequestParam and create simple web calculator.

--- src/main/java/ru/kviz/springcourse/controllers/FirstController.java
+++ src/main/java/ru/kviz/springcourse/controllers/FirstController.java
@@ -1,20 +1,54 @@
 package ru.kviz.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String sureName,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + sureName);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) Integer a,
+                             @RequestParam(value = "b", required = false) Integer b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        double result;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 
 }
